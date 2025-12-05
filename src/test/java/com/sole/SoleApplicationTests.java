0153beaa1d2c345b560fd047aa8acc2c9d006266
cayman031 <cayman031@gmail.com>
2025-12-05 13:28:05 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/sole/SoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
